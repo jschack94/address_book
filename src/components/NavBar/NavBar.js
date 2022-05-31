b9dec50fd43f6c9203727449a9009f4245a989ba
jschack94 <jschack94@gmail.com>
2022-05-31 16:04:40 -0500

    progress on homepage

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './NavBar.css';
+import {Link} from 'react-router-dom'
+
 
 let NavBar = () => {
 return (
 <React.Fragment>
-    <h2>AddContact</h2>
+<header className='navbar'>
+        <div className='navbar__title navbar__item'>
+            <Link to={'/'} className="navbar__title">
+             <i className="fa fa-mobile mobile_icon"/>Contact Manager</Link>
+        </div>
+        <div className='navbar__item'>Settings</div>     
+    </header>
 </React.Fragment>
 
 )
