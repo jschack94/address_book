a3907301e0d1e7290a6b8ab658fb3808edaa2e20
jschack94 <jschack94@gmail.com>
2022-05-31 14:31:07 -0500

    installed font dependency

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
     <h2>Address Book</h2>
+    <i className="fa fa-home"/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+/* Fontawesome icons */
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
